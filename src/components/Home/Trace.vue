3a8f808523d20e60d8b18d5e675b20ba7bc5c319
guojs-github <guoojs@gmail.com>
2018-11-29 10:52:07 +0800

    更新 20181129
    实现主界面【物流跟踪】tab显示实现

--- src/components/Home/Trace.vue
+++ src/components/Home/Trace.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="home-page">
-    物流跟踪
+    <div class='image top'>
+      <img src='../../assets/trace-banner.png'/>
+    </div>
+
+    <div class='section ef-click menu orders-finished' @click='onClickOrdersFinished'>
+      <div class='flex-row'>
+        <div class='image'>
+          <img src='../../assets/orders-finished.png'/>
+        </div>
+        <div class='text-normal label'>已完结订单</div>
+      </div>
+      <img src='../../assets/arrow-right.png'/>
+    </div>
+
+    <div class='section ef-click menu orders-shipping' @click='onClickOrdersShipping'>
+      <div class='flex-row'>
+        <div class='image'>
+          <img src='../../assets/orders-shipping.png'/>
+        </div>
+        <div class='text-normal label'>已发货订单</div>
+      </div>
+      <img src='../../assets/arrow-right.png'/>
+    </div>
+
+    <div class='section ef-click menu orders-query' @click='onClickOrdersQuery'>
+      <div class='flex-row'>
+        <div class='image'>
+          <img src='../../assets/orders-query.png'/>
+        </div>
+        <div class='text-normal label'>运单号查询</div>
+      </div>
+      <img src='../../assets/arrow-right.png'/>
+    </div>
+
   </div>
 </template>
 
@@ -14,11 +47,25 @@ export default {
   components: {
   },
   methods: {
+    onClickOrdersFinished: function (e) {
+      console.log('On click menu orders finished.')
+    },
+
+    onClickOrdersShipping: function (e) {
+      console.log('On click menu orders shipping.')
+    },
+
+    onClickOrdersQuery: function (e) {
+      console.log('On click menu orders query.')
+    }
+
+    /**********************************************/
   }
 }
 </script>
 
 <style scoped src='./Page.css'/>
+<style scoped src='./Trace.css'/>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
