55ccc075b85474c76bef61678365bcde10de2357
guojs-github <guoojs@gmail.com>
2018-12-03 10:21:58 +0800

    20181203 我的界面添加预约入口

--- src/components/Home/Me.vue
+++ src/components/Home/Me.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="home-page">
     <div class='image top background-image'>
+      <div class='row'>
+        <div class='avatar image'>
+          <img src='../../assets/avatar.png'/>
+        </div>
+        <div class='login text-white ef-click'>登录/注册 ></div>
+      </div>
+    </div>
+
+    <div class='booking section'>
+      <div class='row'>
+        <div class='booking row ef-click'>
+          <div class='button-image image'>
+            <img src='../../assets/me-booking.png'>
+          </div>
+          <div class='button-text text-normal'>提送货预约</div>
+        </div>
+        <div class='my-booking row ef-click'>
+          <div class='button-image image'>
+            <img src='../../assets/me-my-booking.png'>
+          </div>
+          <div class='button-text text-normal'>我的预约</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
