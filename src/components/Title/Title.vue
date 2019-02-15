4c7253cb26eb6fbe9fd31f5ac64510d06f94525b
guojs-github <guoojs@gmail.com>
2019-02-15 13:42:00 +0800

    20190215 Vuex练习

--- src/components/Title/Title.vue
+++ src/components/Title/Title.vue
@@ -3,7 +3,7 @@
 		<FormTitle title='Vue实现样例' :showGoback='false'/>
 		<div class='form-body'>
 			<img class='title' src='../../assets/login_bg.png'/>
-			<div class='hint text-light'>请授权您的用户信息</div>
+			<div class='hint text-light'>请授权您的用户信息, c = {{ count }}, a = {{ countAlias }}, p = {{ countPlus }}</div>
 			<div class='authorize button-red ef-click-red' @click='onClickAuthorize'>
 				授权
 			</div>
@@ -12,14 +12,32 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import FormTitle from '@/components/Common/FormTitle'
 export default {
 	name: 'Title',
 
 	data () {
 		return {
+			localCount: 20
 		}
 	},
+	
+	/*
+	computed: {
+		count () {
+			return this.$store.state.count
+		}
+	},
+	*/
+	
+	computed: mapState({
+		count: state => state.count,
+		countAlias: 'count', // 传字符串参数 'count' 等同于 `state => state.count`
+		countPlus (state) {
+			return state.count + this.localCount
+		}
+	}),
 
 	components: {
 		FormTitle
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // 这里似乎是入口
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App' // 组件App
 import router from './router' // 路由器？
 import MintUI from 'mint-ui'
@@ -11,12 +12,26 @@ Vue.config.productionTip = false
 // 开启debug模式
 Vue.config.debug = true
 
-// Mint UI
-Vue.use(MintUI)
+Vue.use(MintUI) // Mint UI
+Vue.use(Vuex) // Vuex
+
+/* Vuex store */
+const store = new Vuex.Store({
+	state: {
+		count: -99
+	},
+
+	mutations: {
+		increment (state) {
+			state.count++
+		}
+	}	
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app', // DOM匹配表达式，作为Vue实例挂载目标
+  store, // 把 store 对象提供给 “store” 选项，这可以把 store 的实例注入所有的子组件
   router, // 指定路由器，挂载在<router-view/>节点下
   components: { App }, // 引用组件表
   template: '<App/>' // 调用App组件替换el指定的DOM对象
