441f9db878b7d5120372a468ba9dc6d65f11a54e
guojs-github <guoojs@gmail.com>
2019-02-18 10:04:25 +0800

    20190218 Getters

--- src/components/Title/Title.vue
+++ src/components/Title/Title.vue
@@ -3,9 +3,17 @@
 		<FormTitle title='Vue实现样例' :showGoback='false'/>
 		<div class='form-body'>
 			<img class='title' src='../../assets/login_bg.png'/>
-			<div class='hint text-light'>请授权您的用户信息, c = {{ count }}, a = {{ countAlias }}, p = {{ countPlus }}</div>
+			<div class='hint text-light'>请授权您的用户信息</div>
 			<div class='authorize button-red ef-click-red' @click='onClickAuthorize'>
 				授权
+			</div>
+			<div>
+				<div>count = {{ count }}</div>
+				<div>count alias = {{ countAlias }}</div>
+				<div>count plus = {{ countPlus }}</div>
+				<div>done todos = {{ doneTodos }}</div>
+				<div>done todos count = {{ doneTodosCount }}</div>
+				<div>done todo(id = 2) = {{ todo(2) }}</div>
 			</div>
 		</div>
 	</div>
@@ -33,9 +41,25 @@ export default {
 	
 	computed: mapState({
 		count: state => state.count,
+
 		countAlias: 'count', // 传字符串参数 'count' 等同于 `state => state.count`
+
 		countPlus (state) {
 			return state.count + this.localCount
+		},
+
+		doneTodos () {
+			return this.$store.getters.doneTodos
+		},
+
+		doneTodosCount () {
+			return this.$store.getters.doneTodosCount
+		},
+		
+		todo () {
+			return (id) => {
+				return this.$store.getters.getTodoById(id)
+			}
 		}
 	}),
 
--- src/main.js
+++ src/main.js
@@ -18,7 +18,27 @@ Vue.use(Vuex) // Vuex
 /* Vuex store */
 const store = new Vuex.Store({
 	state: {
-		count: -99
+		count: -99,
+		todos: [
+			{ id: 1, text: '1...', done: true },
+			{ id: 2, text: '2...', done: false },
+			{ id: 3, text: '3...', done: true }
+		]		
+	},
+	
+	getters: {
+		doneTodos: state => {
+			return state.todos.filter(todo => todo.done)
+		},
+		
+		doneTodosCount: (state, getters) => {
+			return getters.doneTodos.length
+		},
+		
+		getTodoById: (state) => (id) => {
+			return state.todos.find(todo => todo.id === id)
+		}
+
 	},
 
 	mutations: {
